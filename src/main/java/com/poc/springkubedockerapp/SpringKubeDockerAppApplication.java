4a112590178502dd039476f0379768491501e280
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-02-03 21:09:05 +0800

    Init App

---
+++ src/main/java/com/poc/springkubedockerapp/SpringKubeDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.poc.springkubedockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKubeDockerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKubeDockerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/springkubedockerapp/SpringKubeDockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.springkubedockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKubeDockerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
